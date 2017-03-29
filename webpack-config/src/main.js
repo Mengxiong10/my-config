c3732f94f4714a9997ac4d463fc647be503a337f
Mengxiong10 <15623530290@163.com>
2017-03-29 22:55:24 +0800

    update

--- webpack-config/src/main.js
+++ webpack-config/src/main.js
@@ -1,6 +1,10 @@
 
 import {a} from 'test.js'
-// import './test.css'
+import './test.css'
+
+var app = document.getElementById('app')
+
+app.innerHTML = 'ahahldjio566sdfj'
 
 var b = a(3)
 console.log(b)
--- webpack-config/webpack.dev.js
+++ webpack-config/webpack.dev.js
@@ -1,4 +1,55 @@
 
 const webpack = require('webpack')
+const opn = require('opn')
+const webpackDevServer = require('webpack-dev-server')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const webpackBase = require('./webpack.base.js')
 
-// module.exports = 
+const port = 8080
+const uri = 'http://localhost:' + port 
+
+const webpackConfig = merge(webpackBase,{
+  devtool:'#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': '"development"'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    //跳过编译时出错的代码并记录
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new FriendlyErrorsPlugin(),
+  ]
+})
+
+Object.keys(webpackConfig.entry).forEach((name) => {
+  webpackConfig.entry[name] = [
+    "webpack-dev-server/client?" + uri,
+    "webpack/hot/dev-server"
+  ].concat(webpackConfig.entry[name])
+})
+
+
+const compiler = webpack(webpackConfig)
+
+const server = new webpackDevServer(compiler,{
+  publicPath:webpackConfig.output.publicPath,
+  hot: true ,
+  quiet:true,
+})
+
+server.listen(port,function(err) {
+  if (err) {
+    console.error(err)
+    return 
+  }
+  console.log('listen at' + uri)
+  opn(uri)
+})
+
